9f837b50b7347831b31e428f630732cbfd000826
ajpicard913 <Contact@saucedesigns.io>
2020-07-21 13:15:13 -0400

    Initial Commit

---
+++ Dribble-3D-App/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Dribble-3D-App
+//
+//  Created by AJ Picard on 7/21/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dribble-3D-App/Dribble_3D_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dribble_3D_AppApp.swift
+//  Dribble-3D-App
+//
+//  Created by AJ Picard on 7/21/20.
+//
+
+import SwiftUI
+
+@main
+struct Dribble_3D_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
